87351b56bd855ff8f3c1689578eb88732230e984
Xanet2004 <117037654+Xanet2004@users.noreply.github.com>
2022-11-15 11:52:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+//Lortu botoiaren ID-a emen
+xxxxxxxxxxxxxxxxxxxxxxx.onclick = function() {boton1()};
+xxxxxxxxxxxxxxxxxxxxxxx.onclick = function() {boton2()};
+xxxxxxxxxxxxxxxxxxxxxxx.onclick = function() {boton3()};
+
+function boton1(){
+    window.location.href = "/html/nivel1.html";
+}
+function boton2(){
+    window.location.href = "/html/nivel2.html";
+}
+function boton3(){
+    window.location.href = "/html/nivel3.html";
+}
